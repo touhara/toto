33dc5893f6e3a01c8a55bee7db5a8834c0d9e843
unknown <touhara.yuuki@gmail.com>
2012-02-18 18:54:09 +0900

    first toiles

---
+++ src/to/i/le/ToileActivity.java
@@ -0,0 +1,19 @@
+package to.i.le;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+
+
+public class ToileActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+    }
+}
